7d869c32cc76a8ed8400e6049f9248c5ffb544d7
Artem Bulaev <fobozzz@yandex.ru>
2025-03-13 23:53:38 +0300

    continue lesson 5 Selene_1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,45 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+
 
 @pytest.fixture(scope='function', autouse=True)
+
+# Всего есть пять уровней:
+#
+# «function» — для функции (по умолчанию).
+# «class» — для класса.
+# «module» — для модуля (то есть py-файла).
+# «package» — для пакета.
+# «session» — для всей сессии тестирования.
+# Выбор уровня фикстуры зависит от её назначения:
+#
+# Scope='function' — когда нужна инициализация каждый раз перед тестом.
+# Scope='module' — когда нужна инициализация один раз для всех тестов в модуле.
+# Scope='session' — один раз для всей сессии тестирования.
+# Scope='class' — когда нужна инициализация один раз для всех тестов в классе.
 
 def browser_management():
     browser.config.driver_name = 'edge'
     browser.config.base_url = 'https://todomvc.com/examples/emberjs'
+    # browser.config.timeout = 2.0
 
+    # driver_options = webdriver.FirefoxOptions()
+    '''
+    ↑ if we would want to use Firefox with custom browser options instead of Chrome
+    '''
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless') # запуск браузера в невидимом режиме
+
+    # browser.config.driver = webdriver.Chrome(
+    #     service=ChromeService(executable_path=ChromeDriverManager().install()),
+    #     options=driver_options,
+    # )
+    # ↑ однажды нам понадобится что-то подобное для какой-нибудь сложной настройки браузера
+    # или поддержки какого-нибудь конкретного браузера или драйвера
+    # ↓ но пока достаточно просто передать параметры драйвера в конфигурацию Selene
+
+    browser.config.driver_options = driver_options
 
     yield # передает управление тесту
 
--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -1,13 +1,18 @@
 from selene import browser, have, be
 
+# предимпорт в файле match: have, be
+# from selene_in_action.conditions import match
+# и вместо have, be всегда использовать match
 
 
 def test_complete_todo():
 
-
     browser.open('/todomvc/dist/')
     browser.element('.new-todo').should(be.blank)
+
     browser.element('.new-todo').type('a').press_enter()
     browser.element('.new-todo').type('b').press_enter()
     browser.element('.new-todo').type('c').press_enter()
-    browser.all('.toggle').should(have.size(3))
+    browser.all('.todo-list>li').with_(timeout=4.0).should(have.size(3)) # установка опции отличной от опции конфига
+    #browser.all('.todo-list>li').with_(timeout=browser.config.driver_options*2).should(have.size(3))
+
