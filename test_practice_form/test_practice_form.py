c35656d7d44af64df7a89fcf2ba3a71966bc82f6
Artem Bulaev <fobozzz@yandex.ru>
2025-03-26 22:50:19 +0300

    add code HW lesson 5 Selene_1

--- test_practice_form/test_practice_form.py
+++ test_practice_form/test_practice_form.py
@@ -1,7 +1,8 @@
-from pickletools import optimize
+from selene import browser, have
+import os
 
-from click import option
-from selene import browser, have, be
+from selene.core import command
+
 
 def test_practice_form():
     browser.open("https://demoqa.com/automation-practice-form")
@@ -14,24 +15,26 @@ def test_practice_form():
     browser.element("[for=gender-radio-1]").click()
     browser.element("#userNumber").type("89649990000")
     browser.element("#dateOfBirthInput").click()
-    browser.element(".react-datepicker__month-select").element([option("4")]).click()
-    browser.element(".react-datepicker__year-select").element([option("1990")]).click()
+    browser.element(".react-datepicker__month-select").element('[value="4"]').click()
+    browser.element(".react-datepicker__year-select").element('[value="1990"]').click()
     browser.element(".react-datepicker__day--010").click()
     browser.element("#subjectsInput").type("Maths").press_enter()
     browser.element("#subjectsInput").type("bio").press_enter()
     browser.element("#subjectsInput").type("ch").press_enter()
-    browser.element("#hobbiesWrapper").element(have.exact_text("Sports")).click()
-    browser.element("#hobbiesWrapper").element(have.exact_text("Music")).click()
-    browser.element("#uploadPicture").uploadFromClasspath("img/Test.jpg")
+    browser.element("#submit").wait.for_scroll_into_view()
+
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('Test.jpg'))
     browser.element("#currentAddress").type("Penza")
     browser.element("#state").click()
-    browser.element("#stateCity-wrapper").element(have.exact_text("NCR")).click()
+    browser.element("#react-select-3-option-0").click()
     browser.element("#city").click()
-    browser.element("#stateCity-wrapper").element(have.exact_text("Delhi")).click()
+    browser.element("#react-select-4-option-0").click()
     browser.element("#submit").click()
 
     browser.element(".table-hover").should(have.exact_texts('Artem Bulaev', 'Fooolll@test.com',
                                  'Male', '8964999000'))
-    browser.element("#closeLargeModal").click() # прокрутка  для видимости кнопки
+    browser.element("#closeLargeModal").click() # прокрутка для видимости кнопки
 
     browser.close()
